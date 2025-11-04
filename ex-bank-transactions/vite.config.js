ca277b6fe11dc328167b037e07a5eb4f125d83eb
Amir Malek <amir.malekfarnood@gmail.com>
2025-11-04 20:44:42 +0330

    fix vite.config.js

--- ex-bank-transactions/vite.config.js
+++ ex-bank-transactions/vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'transactions.html'
+      }
+    }
+  },
+  server: {
+    open: '/transactions.html'
+  }
 })
